fb0d2ea4285e88419332b7fe7e95047948827d81
Jan Nicklas <j.nicklas@me.com>
2013-12-10 15:47:27 +0100

    Added a blacklist. Fixes #1

--- background.js
+++ background.js
@@ -1,25 +1,66 @@
-if(localStorage["tabs"] === undefined){
+if (localStorage["tabs"] === undefined) {
 	localStorage["tabs"] = true;
 }
-chrome.windows.getCurrent({},function(w){
-	var mainwindow = w.id;
-	chrome.windows.onCreated.addListener(function(w){
-		if(!localStorage["tabs"] || w.type == "popup"){
-			if(!localStorage["ask"] || prompt("Do you really want to merge this tab?")){				
-				chrome.windows.get(w.id,{populate:true},function(w){
-					chrome.tabs.move(w.tabs[0].id,{windowId:mainwindow,index:-1},function(){
-						chrome.tabs.update(w.tabs[0].id,{active:true});
-					});
-				});
-			}
-		}
-	});
 
-	chrome.windows.onFocusChanged.addListener(function(w){
-		chrome.windows.get(w,{},function(w){		
-			if(w.type == "normal"){
-				mainwindow = w.id;
+if (localStorage["blacklist"] === undefined) {
+	// Regular expressions of blacklisted urls
+	localStorage["blacklist"] = [
+		/^chrome-devtools:/
+	];
+}
+
+/**
+ * Returns true if the given string is on the local storage blacklist
+ *
+ * @param url
+ * @returns {boolean}
+ */
+function isBlacklisted(url) {
+	return Array.prototype.some.call(localStorage["blacklist"], function (blacklistRegExp) {
+		return url.match(blacklistRegExp);
+	});
+}
+
+/**
+ * Returns the current main window id
+ *
+ * @returns {number}
+ */
+var getMainWindowId = (function () {
+	var mainWindowId;
+	// get the initial main window id
+	chrome.windows.getCurrent({}, function (window) {
+		mainWindowId = window.id;
+	});
+	// keep the main window id updated
+	chrome.windows.onFocusChanged.addListener(function (windowId) {
+		chrome.windows.get(windowId, {}, function (window) {
+			if (window !== undefined && window.type === "normal") {
+				mainWindowId = window.id;
 			}
 		});
 	});
+	return function () {
+		return mainWindowId;
+	}
+}());
+
+// Wait for popups
+chrome.windows.onCreated.addListener(function (window) {
+	if (getMainWindowId()
+	 && (!localStorage["tabs"] || window.type === "popup")
+	 && (!localStorage["ask"] || prompt("Do you really want to merge this tab?"))) {
+		// Get all tabs of the new window
+		chrome.windows.get(window.id, {populate: true}, function (window) {
+			Array.prototype.forEach.call(window.tabs, function (tab) {
+				// Check if the tab url is blacklisted
+				if (!isBlacklisted(tab.url)) {
+					// Move the tab to the main window
+					chrome.tabs.move(tab.id, {windowId: getMainWindowId(), index: -1}, function () {
+						chrome.tabs.update(tab.id, {active: true});
+					});
+				}
+			});
+		});
+	}
 });
